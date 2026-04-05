46e68a4c9c38cc8d7c563e0bbf21f3193b303a7a
somehow08 <125090038@link.cuhk.edu.cn>
2026-04-06 00:20:27 +0800

    修改了原有路径

--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-import os
-import sys
-
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-SRC_PATH = os.path.join(PROJECT_ROOT, "src")
-if SRC_PATH not in sys.path:
-    sys.path.insert(0, SRC_PATH)
-
-from weibo_spider.main import main
+from src.main import main
 
 
 if __name__ == "__main__":
